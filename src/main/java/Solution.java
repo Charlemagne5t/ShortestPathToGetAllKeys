f2951f8f18d46e7dea2b026870b0ecdd1c3e21c5
alek <wait82985@gmail.com>
2023-06-29 10:40:05 +0600

    initial commit. 3 test cases

---
+++ src/main/java/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int shortestPathAllKeys(String[] grid) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void shortestPathAllKeysTest1() {
+        String[] grid = {"@.a..", "###.#", "b.A.B"};
+        int expected = 8;
+        int actual = new Solution().shortestPathAllKeys(grid);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortestPathAllKeysTest2() {
+        String[] grid = {"@..aA", "..B#.", "....b"};
+        int expected = 6;
+        int actual = new Solution().shortestPathAllKeys(grid);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shortestPathAllKeysTest3() {
+        String[] grid = {"@Aa"};
+        int expected = -1;
+        int actual = new Solution().shortestPathAllKeys(grid);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
